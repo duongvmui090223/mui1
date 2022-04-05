9ba2e063e456f214eae173782d4c2906ca88ffd3
mui1 <duongvmui09022@gmail.com>
2022-04-05 20:40:30 +0700

    first commit

---
+++ app/Http/Controllers/web2.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class secondController extends Controller
+{
+    public function web2() {
+        return view ('web2');
+    }
+}
---
+++ app/Http/Controllers/web3.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class threeController extends Controller
+{
+    public function web3() {
+        return view ('web3');
+    }
+}
